d4c18af80cb8c79ea3bfa2ae1e54fa7c9bfbb816
Zhen Wang <wangzhen_ok@bupt.edu.cn>
2020-03-12 22:44:43 +0800

    train scripts

--- model/attention_ocr.py
+++ model/attention_ocr.py
@@ -1,3 +1,5 @@
+import math
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -54,93 +56,6 @@ class MyIncept(nn.Module):
         return x
 
 
-class Attn(torch.nn.Module):
-    def __init__(self, method, hidden_size):
-        super(Attn, self).__init__()
-        self.method = method
-        if self.method not in ['dot', 'general', 'concat']:
-            raise ValueError(self.method, "is not an appropriate attention method.")
-        self.hidden_size = hidden_size
-        if self.method == 'general':
-            self.attn = torch.nn.Linear(self.hidden_size, hidden_size)
-        elif self.method == 'concat':
-            self.attn = torch.nn.Linear(self.hidden_size * 2, hidden_size)
-            self.v = torch.nn.Parameter(torch.FloatTensor(hidden_size))
-
-    def dot_score(self, hidden, encoder_output):
-        return torch.sum(hidden * encoder_output, dim=2)
-
-    def general_score(self, hidden, encoder_output):
-        energy = self.attn(encoder_output)
-        return torch.sum(hidden * energy, dim=2)
-
-    def concat_score(self, hidden, encoder_output):
-        energy = self.attn(torch.cat((hidden.expand(encoder_output.size(0), -1, -1), encoder_output), 2)).tanh()
-        return torch.sum(self.v * energy, dim=2)
-
-    def forward(self, hidden, encoder_outputs):
-        # Calculate the attention weights (energies) based on the given method
-        if self.method == 'general':
-            attn_energies = self.general_score(hidden, encoder_outputs)
-        elif self.method == 'concat':
-            attn_energies = self.concat_score(hidden, encoder_outputs)
-        elif self.method == 'dot':
-            attn_energies = self.dot_score(hidden, encoder_outputs)
-
-        # Transpose max_length and batch_size dimensions
-        attn_energies = attn_energies.t()
-
-        # Return the softmax normalized probability scores (with added dimension)
-        return F.softmax(attn_energies, dim=1).unsqueeze(1)
-
-
-class AttnDecoderRNN(nn.Module):
-    def __init__(self, attn_model, hidden_size, output_size, n_layers=1, dropout=0.1):
-        super(AttnDecoderRNN, self).__init__()
-
-        # Keep for reference
-        self.attn_model = attn_model
-        self.hidden_size = hidden_size
-        self.output_size = output_size
-        self.n_layers = n_layers
-        self.dropout = dropout
-
-        # Define layers
-        self.embedding = nn.Embedding(self.output_size, self.hidden_size)
-        self.embedding_dropout = nn.Dropout(dropout)
-        self.gru = nn.GRU(hidden_size, hidden_size, n_layers, dropout=(0 if n_layers == 1 else dropout))
-        self.concat = nn.Linear(hidden_size * 2, hidden_size)
-        self.out = nn.Linear(hidden_size, output_size)
-
-        self.attn = Attn(attn_model, hidden_size)
-
-    def forward(self, input_step, last_hidden, encoder_outputs):
-        # Note: we run this one step (word) at a time
-        # Get embedding of current input word
-        b = last_hidden.size(1)
-        embedded = self.embedding(input_step).view(1, b, -1)
-        embedded = self.embedding_dropout(embedded)
-        # Forward through unidirectional GRU
-        rnn_output, hidden = self.gru(embedded, last_hidden)
-        # Calculate attention weights from the current GRU output
-        attn_weights = self.attn(rnn_output, encoder_outputs)
-        # Multiply attention weights to encoder outputs to get new "weighted sum" context vector
-        context = attn_weights.bmm(encoder_outputs.transpose(0, 1))
-        # Concatenate weighted context vector and GRU output using Luong eq. 5
-        rnn_output = rnn_output.squeeze(0)
-        context = context.squeeze(1)
-        concat_input = torch.cat((rnn_output, context), 1)
-        concat_output = torch.tanh(self.concat(concat_input))
-        # Predict next word using Luong eq. 6
-        output = self.out(concat_output)
-        output = F.log_softmax(output, dim=1)
-        # Return output and final hidden state
-        return output, hidden
-
-    def init_hidden(self, batch_size):
-        return torch.zeros((self.n_layers, batch_size, self.hidden_size))
-
-
 class OneHot(nn.Module):
     def __init__(self, depth):
         super(OneHot, self).__init__()
@@ -152,3 +67,160 @@ class OneHot(nn.Module):
     def forward(self, input_):
         return self.emb(input_)
 
+
+class Attention(nn.Module):
+    def __init__(self, hidden_size):
+        super(Attention, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.attn = nn.Linear(hidden_size * 2, hidden_size)
+        self.v = nn.Parameter(torch.rand(hidden_size), requires_grad=True)
+        stdv = 1. / math.sqrt(self.v.size(0))
+        self.v.data.uniform_(-stdv, stdv)
+
+    def forward(self, hidden, encoder_outputs):
+        timestep = encoder_outputs.size(1)
+        h = hidden.expand(timestep, -1, -1).transpose(0, 1)
+        attn_energies = self.score(h, encoder_outputs)
+        return attn_energies.softmax(2)
+
+    def score(self, hidden, encoder_outputs):
+        energy = torch.tanh(self.attn(torch.cat([hidden, encoder_outputs], 2)))
+        energy = energy.transpose(1, 2)
+        v = self.v.expand(encoder_outputs.size(0), -1).unsqueeze(1)
+        energy = torch.bmm(v, energy)
+        return energy
+
+
+class Decoder(nn.Module):
+    def __init__(self, vocab_size, max_len, hidden_size, sos_id, eos_id, n_layers=1):
+        super(Decoder, self).__init__()
+
+        self.vocab_size = vocab_size
+        self.max_len = max_len
+        self.hidden_size = hidden_size
+        self.sos_id = sos_id
+        self.eos_id = eos_id
+        self.n_layers = n_layers
+
+        self.emb = nn.Embedding(vocab_size, hidden_size)
+        self.attention = Attention(hidden_size)
+        self.rnn = nn.GRU(hidden_size * 2, hidden_size, n_layers)
+
+        self.out = nn.Linear(hidden_size, vocab_size)
+
+    def forward_step(self, input_, last_hidden, encoder_outputs):
+        emb = self.emb(input_.transpose(0, 1))
+        attn = self.attention(last_hidden, encoder_outputs)
+        context = attn.bmm(encoder_outputs).transpose(0, 1)
+        rnn_input = torch.cat((emb, context), dim=2)
+
+        outputs, hidden = self.rnn(rnn_input, last_hidden)
+
+        if outputs.requires_grad:
+            outputs.register_hook(lambda x: x.clamp(min=-10, max=10))
+
+        outputs = self.out(outputs.contiguous().squeeze(0)).log_softmax(1)
+
+        return outputs, hidden
+
+    def forward(self, inputs=None, encoder_hidden=None, encoder_outputs=None,
+                teacher_forcing_ratio=0):
+        inputs, batch_size, max_length = self._validate_args(
+            inputs, encoder_hidden, encoder_outputs, teacher_forcing_ratio)
+
+        use_teacher_forcing = True if torch.rand(1).item() < teacher_forcing_ratio else False
+
+        outputs = []
+
+        self.rnn.flatten_parameters()
+
+        decoder_hidden = torch.zeros(1, batch_size, self.hidden_size, device=encoder_outputs.device)
+
+        def decode(step_output):
+            symbols = step_output.topk(1)[1]
+            return symbols
+
+        if use_teacher_forcing:
+            for di in range(max_length):
+                decoder_input = inputs[:, di].unsqueeze(1)
+
+                decoder_output, decoder_hidden = self.forward_step(
+                    decoder_input, decoder_hidden, encoder_outputs)
+
+                step_output = decoder_output.squeeze(1)
+                outputs.append(step_output)
+        else:
+            decoder_input = inputs[:, 0].unsqueeze(1)
+            for di in range(max_length):
+                decoder_output, decoder_hidden = self.forward_step(
+                    decoder_input, decoder_hidden, encoder_outputs
+                )
+
+                step_output = decoder_output.squeeze(1)
+                outputs.append(step_output)
+
+                symbols = decode(step_output)
+                decoder_input = symbols
+
+        outputs = torch.stack(outputs).permute(1, 0, 2)
+
+        return outputs, decoder_hidden
+
+    def _validate_args(self, inputs, encoder_hidden, encoder_outputs, teacher_forcing_ratio):
+        batch_size = encoder_outputs.size(0)
+
+        if inputs is None:
+            assert teacher_forcing_ratio == 0
+
+            inputs = torch.full((batch_size, 1), self.sos_id, dtype=torch.long, device=encoder_outputs.device)
+
+            max_length = self.max_len
+        else:
+            max_length = inputs.size(1) - 1
+
+        return inputs, batch_size, max_length
+
+
+class OCR(nn.Module):
+    def __init__(self, img_width, img_height, nh, n_classes, max_len, SOS_token, EOS_token):
+        super(OCR, self).__init__()
+
+        self.incept = MyIncept()
+
+        f = self.incept(torch.rand(1, 3, img_height, img_width))
+
+        self._fh = f.size(2)
+        self._fw = f.size(3)
+        print('Model feature size:', self._fh, self._fw)
+
+        self.onehot_x = OneHot(self._fh)
+        self.onehot_y = OneHot(self._fw)
+        self.encode_emb = nn.Linear(288 + self._fh + self._fw, nh)
+        self.decoder = Decoder(n_classes, max_len, nh, SOS_token, EOS_token)
+
+        self._device = 'cpu'
+
+    def forward(self, input_, target_seq=None, teacher_forcing_ratio=0):
+        device = input_.device
+        b, c, h, w = input_.size()
+        encoder_outputs = self.incept(input_)
+
+        b, fc, fh, fw = encoder_outputs.size()
+
+        x, y = torch.meshgrid(torch.arange(fh, device=device), torch.arange(fw, device=device))
+
+        h_loc = self.onehot_x(x)
+        w_loc = self.onehot_y(y)
+
+        loc = torch.cat([h_loc, w_loc], dim=2).unsqueeze(0).expand(b, -1, -1, -1)
+
+        encoder_outputs = torch.cat([encoder_outputs.permute(0, 2, 3, 1), loc], dim=3)
+        encoder_outputs = encoder_outputs.contiguous().view(b, -1, 288 + self._fh + self._fw)
+
+        encoder_outputs = self.encode_emb(encoder_outputs)
+
+        decoder_outputs, decoder_hidden = self.decoder(target_seq, encoder_outputs=encoder_outputs,
+                                                       teacher_forcing_ratio=teacher_forcing_ratio)
+
+        return decoder_outputs
--- train.py
+++ train.py
@@ -2,272 +2,124 @@ import random
 import time
 import pickle
 
+from tqdm import tqdm
+
 from PIL import Image
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader, Sampler
-
-from ignite.engine import Engine, Events, create_supervised_evaluator, _prepare_batch
-from ignite.handlers import EarlyStopping, Timer
+from torch.utils.data import DataLoader
 
 from torchvision import transforms
 
-from model.attention_ocr import *
-from utils.data_loader import OCRDataset, OnePicSampler
-from utils.weight_init import weight_init
+from model.attention_ocr import OCR
+from utils.dataset import CaptchaDataset
+from utils.train_util import train_batch, eval_batch
 
 device = 'cuda'
-use_pretrained_incept = False
 
-img_height = 50
-img_width = 256
-n_channels = 3
 
-preprocess_workers = 30
+def main():
+    img_width = 160
+    img_height = 60
+    max_len = 4
 
-batch_size = 128
-max_char_per_img = 5
+    nh = 512
 
-MAX_ATTEN_LENGTH = 100
+    teacher_forcing_ratio = 0.5
 
-n_epoch = 50
-print_interval = 2000
+    batch_size = 32
 
-random_state = 42
+    lr = 3e-4
+    n_epoch = 100
 
-teacher_forcing_ratio = 0.5
+    n_works = 8
+    save_checkpoint_every = 5
 
-with open('chars.txt', 'r') as f:
-    chars = f.readlines()[0]
+    ds_train = CaptchaDataset(img_width, img_height, 10000, max_len)
+    ds_test = CaptchaDataset(img_width, img_height, 1000, max_len)
 
-n_classes = len(chars) + 3  # reserve for SOS, UNKNOWN and EOS
-SOS_token = 0
-EOS_token = len(chars) + 2
+    tokenizer = ds_train.tokenizer
 
-incept = MyIncept().to(device=device)
-att = AttnDecoderRNN('dot', 288 + 3 + MAX_ATTEN_LENGTH // 3 + 1, n_classes).to(device=device)
+    train_loader = DataLoader(ds_train, batch_size=batch_size, shuffle=True, num_workers=n_works)
+    test_loader = DataLoader(ds_test, batch_size=batch_size, shuffle=False, num_workers=n_works)
 
-onehot_x = OneHot(3).to(device=device)
-onehot_y = OneHot(MAX_ATTEN_LENGTH // 3 + 1).to(device=device)
+    model = OCR(img_width, img_height, nh, tokenizer.n_token,
+                max_len + 1, tokenizer.SOS_token, tokenizer.EOS_token).to(device=device)
 
-if use_pretrained_incept:
-    # load pretrained weights
-    incept.load_state_dict(torch.load('/path/'), restrict=False)
-    incept.requires_grad = False
-else:
-    incept.appy(weight_init)
+    load_weights = torch.load('./inception_v3_google-1a9a5a14.pth')
 
-att.apply(weight_init)
+    names = set()
+    for k, w in model.incept.named_children():
+        names.add(k)
 
+    weights = {}
+    for k, w in load_weights.items():
+        if k.split('.')[0] in names:
+            weights[k] = w
 
-def train_batch(input_tensor, target_tensor, optimizer, criterion, max_length=MAX_ATTEN_LENGTH):
-    img_feature = incept(input_tensor)
-    b, c, h, w = img_feature.size()
-    img_feature = img_feature.permute(0, 2, 3, 1)  # h, w, c
+    model.incept.load_state_dict(weights)
 
-    x, y = torch.meshgrid(torch.arange(h, device=device), torch.arange(w, device=device))
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    crit = nn.NLLLoss().cuda()
 
-    h_loc = onehot_x(x)
-    w_loc = onehot_y(y)
+    def train_epoch():
+        sum_loss_train = 0
+        n_train = 0
+        sum_acc = 0
+        sum_sentence_acc = 0
 
-    loc = torch.cat([h_loc, w_loc], dim=2).unsqueeze(0).expand(b, -1, -1, -1)
+        for bi, batch in enumerate(tqdm(train_loader)):
+            x, y = batch
+            x = x.to(device=device)
+            y = y.to(device=device)
 
-    img_feature = torch.cat([img_feature, loc], dim=3)
+            loss, acc, sentence_acc = train_batch(x, y, model, optimizer,
+                                                  crit, teacher_forcing_ratio, max_len,
+                                                  tokenizer)
 
-    img_feature = img_feature.contiguous().view(b, h * w, -1).permute(1, 0, 2)
+            sum_loss_train += loss
+            sum_acc += acc
+            sum_sentence_acc += sentence_acc
 
-    tmp = torch.zeros((max_length, b, img_feature.size(2)), device=device)
-    tmp[:h * w] = img_feature
-    img_feature = tmp
+            n_train += 1
 
-    optimizer.zero_grad()
+        return sum_loss_train / n_train, sum_acc / n_train, sum_sentence_acc / n_train
 
-    # target has identical length
-    b, w = target_tensor.size()
-    tmp = torch.zeros((max_length, b), dtype=torch.long, device=device)
-    tmp[:w, :] = target_tensor.permute(1, 0)
-    tmp[w, :] = EOS_token
+    def eval_epoch():
+        sum_loss_eval = 0
+        n_eval = 0
+        sum_acc = 0
+        sum_sentence_acc = 0
 
-    target_tensor = tmp
+        for bi, batch in enumerate(tqdm(test_loader)):
+            x, y = batch
+            x = x.to(device=device)
+            y = y.to(device=device)
 
-    target_length = w + 1
+            loss, acc, sentence_acc = eval_batch(x, y, model, crit, max_len, tokenizer)
 
-    loss = 0
+            sum_loss_eval += loss
+            sum_acc += acc
+            sum_sentence_acc += sentence_acc
 
-    decoder_input = torch.full((b,), SOS_token, dtype=torch.long, device=device)
+            n_eval += 1
 
-    decoder_hidden = att.init_hidden(b)
+        return sum_loss_eval / n_eval, sum_acc / n_eval, sum_sentence_acc / n_eval
 
-    use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
+    for epoch in range(n_epoch):
+        train_loss, train_acc, train_sentence_acc = train_epoch()
+        eval_loss, eval_acc, eval_sentence_acc = eval_epoch()
 
-    if use_teacher_forcing:
-        # Teacher forcing: Feed the target as the next input
-        for di in range(target_length):
-            decoder_output, decoder_hidden = att(
-                decoder_input, decoder_hidden, img_feature)
+        print("Epoch %d" % epoch)
+        print('train_loss: %.4f, train_acc: %.4f, train_sentence: %.4f' % (train_loss, train_acc, train_sentence_acc))
+        print('eval_loss:  %.4f, eval_acc:  %.4f, eval_sentence:  %.4f' % (eval_loss, eval_acc, eval_sentence_acc))
 
-            loss += criterion(decoder_output, target_tensor[di])
-            decoder_input = target_tensor[di]  # Teacher forcing
+        if epoch % save_checkpoint_every == 0 and epoch > 0:
+            print('saving checkpoint...')
+            torch.save(model.state_dict(), './chkpoint/time_%s_epoch_%s.pth' % (time.strftime('%Y-%m-%d_%H-%M-%S'), epoch))
 
-    else:
-        # Without teacher forcing: use its own predictions as the next input
-        for di in range(target_length):
-            decoder_output, decoder_hidden = att(
-                decoder_input, decoder_hidden, img_feature)
-            topv, topi = decoder_output.topk(1)
-            decoder_input = topi.squeeze().detach()  # detach from history as input
 
-            loss += criterion(decoder_output, target_tensor[di])
-            if decoder_input[0].item() == EOS_token:
-                break
-
-    loss.backward()
-
-    optimizer.step()
-
-    return loss.item() / target_length
-
-
-def inference(input_tensor, max_length=MAX_ATTEN_LENGTH):
-    att.eval()
-    with torch.no_grad():
-        img_feature = incept(input_tensor)
-        b, c, h, w = img_feature.size()
-        assert b == 1
-        img_feature = img_feature.permute(0, 2, 3, 1)  # h, w, c
-
-        x, y = torch.meshgrid(torch.arange(h, device=device), torch.arange(w, device=device))
-
-        h_loc = onehot_x(x)
-        w_loc = onehot_y(y)
-
-        loc = torch.cat([h_loc, w_loc], dim=2).unsqueeze(0).expand(b, -1, -1, -1)
-
-        img_feature = torch.cat([img_feature, loc], dim=3)
-
-        img_feature = img_feature.contiguous().view(b, h * w, -1).permute(1, 0, 2)
-
-        tmp = torch.zeros((max_length, b, img_feature.size(2)), device=device)
-        tmp[:h * w] = img_feature
-        img_feature = tmp
-
-        decoder_input = torch.full((b,), SOS_token, dtype=torch.long, device=device)
-
-        decoder_hidden = att.init_hidden(b)
-
-        decoded_words = []
-        #         decoder_attentions = torch.zeros(max_length, max_length)
-
-        for di in range(max_length):
-            decoder_output, decoder_hidden = att(
-                decoder_input, decoder_hidden, img_feature)
-            #             decoder_attentions[di] = decoder_attention.data
-            topv, topi = decoder_output.data.topk(1)
-            decoded_words.append(topi.item())
-            if topi.item() == EOS_token:
-                break
-
-            decoder_input = topi.squeeze().detach()
-
-        return decoded_words
-
-
-optimizer = optim.Adadelta(att.parameters())
-# optimizer = optim.Adam(att.parameters(), lr=lr)
-nll_loss = nn.NLLLoss().to(device=device)
-
-
-def create_trainer(optimizer, loss_fn, device=device):
-    def _update(engine, batch):
-        incept.eval()
-        att.train()
-        x, y = _prepare_batch(batch, device=device)
-        loss = train_batch(x, y, optimizer, loss_fn)
-        engine.state.loss_total += loss
-        return loss
-
-    engine = Engine(_update)
-
-    return engine
-
-
-img_trans = transforms.Compose([
-    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
-    transforms.RandomRotation(15, resample=Image.NEAREST),
-    transforms.Resize(img_height),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=(0.5,), std=(0.5,))
-])
-
-
-def train_on_char_per_img(df, char_per_img):
-    print("=================================================================")
-    print('Max char length per img: ', char_per_img)
-
-    df_train = df[df.content.str.len() <= char_per_img]
-
-    print('Dataset len: ', len(df))
-
-    ds_train = OCRDataset(df_train, './imgs', chars, char_per_img,
-                          img_transform=img_trans)
-
-    train_loader = DataLoader(ds_train, batch_size=batch_size,
-                              sampler=OnePicSampler(print_interval * 3, batch_size, df_train, chars),
-                              shuffle=False, num_workers=preprocess_workers)
-
-    for sample in train_loader:
-        break
-
-    "---------------------------------------------"
-
-    trainer = create_trainer(optimizer, nll_loss, device=device)
-
-    @trainer.on(Events.STARTED)
-    def init_loss(trainer):
-        trainer.state.loss_total = 0
-        trainer.state.loss = 0
-
-    def score_function(engine):
-        loss = engine.state.loss
-        return -loss
-
-    es_handler = EarlyStopping(patience=500, score_function=score_function, trainer=trainer)
-
-    trainer.add_event_handler(Events.COMPLETED, es_handler)
-
-    timer = Timer(average=True)
-    timer.attach(trainer,
-                 start=Events.EPOCH_STARTED,
-                 resume=Events.ITERATION_STARTED,
-                 pause=Events.ITERATION_COMPLETED,
-                 step=Events.ITERATION_COMPLETED)
-
-    @trainer.on(Events.ITERATION_COMPLETED)
-    def log_training_loss(trainer):
-        if trainer.state.iteration % print_interval == 1:
-            trainer.state.loss = trainer.state.loss_total / print_interval
-            trainer.state.loss_total = 0
-            print("Epoch[{}] Iter[{}] Loss: {:.2f} AvgTime {:.2f}".format(
-                trainer.state.epoch, trainer.state.iteration, trainer.state.loss, timer.value()))
-
-    @trainer.on(Events.EPOCH_COMPLETED)
-    def log_training_results(trainer):
-        print("Training Results - Epoch: {}  Avg loss: {:.2f}"
-              .format(trainer.state.epoch, trainer.state.loss))
-
-    print("==================================================================")
-    print('Training...')
-    trainer.run(train_loader, max_epochs=n_epoch)
-
-    print('saving model...')
-    ts = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-
-    torch.save(att.state_dict(), './models/' + 'attn_pos_' + str(char_per_img) + '_' + ts + '.pth')
-
-
-df = pickle.load(open('./df_select_after_seg.p', 'rb'))
-
-for i in sorted(df.content.str.len().unique()):
-    train_on_char_per_img(df, i)
+if __name__ == '__main__':
+    main()
